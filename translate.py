bc5e6a5579fa6302505cb05b9c8716789ed4fcdd
Alexander Lazaris <alex.lazaris617@gmail.com>
2017-06-05 21:44:09 +1000

    Got this to a good state. Does the job.

---
+++ translate.py
@@ -0,0 +1,21 @@
+# Imports the Google Cloud client library
+from google.cloud import translate
+from oauth2client.client import GoogleCredentials
+credentials = GoogleCredentials.get_application_default()
+
+
+# Instantiates a client
+translate_client = translate.Client()
+
+# The text to translate
+text = u'Hello, world!'
+# The target language
+target = 'ru'
+
+# Translates some text into Russian
+translation = translate_client.translate(
+    text,
+    target_language=target)
+
+print(u'Text: {}'.format(text))
+print(u'Translation: {}'.format(translation['translatedText']))
